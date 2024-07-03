f6e0d21ab009a0a67d0c9a2285bf0eec0c2f881a
soilihi ambdouroihmane <soilihiambdouroihmane@gmail.com>
2024-07-03 06:55:46 +0000

    update

---
+++ main.js
@@ -0,0 +1,33 @@
+/**
+ * Type primitives
+ * - String
+ * - number
+ * - bigint
+ * - boolean
+ * - null
+ * - undefined 
+ * - Symbol
+ * 
+ * Refersnces type
+ * - objet
+ * - (array)
+ */
+
+let maVariable;
+
+console.log(maVariable); // Ici on aura comme variable undifined car la variable n'est pas assigné
+
+maVariable = "Melvyn";
+
+console.log(maVariable); //Ici on aura comme réponse "Malvyn"
+
+//Les variable en JS sont dynamiques c'est à dire ça peut changer d'un type à un autre
+
+maVariable = 144; //Ici on aura 144 en sortie 
+
+//Déclarer un constante 
+const myConstante = "Ambdou";
+
+console.log(myConstante); //Ici le variable a comme valeur : Ambdou
+
+// myConstante = 144 //Erreur myConstante est un variable constante 
